152e06a802795229e3263575f170f3c881522db5
antonyMeow <flewdash777@gmail.com>
2022-08-06 17:40:19 +0300

    refactor

--- src/js/simpleAlgorithms/sorts/insertionSort.js
+++ src/js/simpleAlgorithms/sorts/insertionSort.js
@@ -6,12 +6,14 @@ console.log(result);
 function insertionSort (arr) {
   for (let i = 1; i < arr.length; i++) {
     let j = i;
+
     while (j > 0 && arr[j-1] > arr[j]) {
       const key = arr[j];
       arr[j] = arr[j-1];
       arr[j-1] = key;
-      j -= 1;
+      j--;
     }
   }
+  
   return arr;
 }
