547df5d05b6c6a86010a5c00d09585a51166499d
antonyMeow <flewdash777@gmail.com>
2022-08-07 01:18:08 +0300

    add addBitArrays function

--- src/js/simpleAlgorithms/recursions.js
+++ src/js/simpleAlgorithms/recursions.js
@@ -6,7 +6,10 @@ const sumOfArr = arr => arr.length == 0 ? 0 : arr.shift() + sumOfArr(arr);
 
 const arrLength = arr => arr[0] ? arr.shift() * 0 + 1 + arrLength(arr) : 1;
 
+const addBitArrays = (a, b) => [+(parseInt(a.join(""), 2) + parseInt(b.join(""), 2)).toString(2)];
+
 console.log(factorial(5));
 console.log(gcd(140, 12));
 console.log(sumOfArr([10,10,10,10,10]));
 console.log(arrLength([1,2,3,4,5]));
+console.log(addBitArrays([1,0,1,1], [0,1,1,0]))
