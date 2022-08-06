5e0edf399ee35f45e848bca3f3a38f250bd23444
antonyMeow <flewdash777@gmail.com>
2022-08-06 17:36:55 +0300

    add insertionSort.js

---
+++ src/js/simpleAlgorithms/insertionSort.js
@@ -0,0 +1,17 @@
+const arr = [243,43,1,254,789,5,3,68,90,2,4,124,-342,-23,-1,325,-8,10];
+const result = insertionSort(arr);
+
+console.log(result);
+
+function insertionSort (arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let j = i;
+    while (j > 0 && arr[j-1] > arr[j]) {
+      const key = arr[j];
+      arr[j] = arr[j-1];
+      arr[j-1] = key;
+      j -= 1;
+    }
+  }
+  return arr;
+}
